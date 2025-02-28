d6f265223074531a3c2a4f0dfac08283e89c6e07
gabic12 <31183067+gabic12@users.noreply.github.com>
2025-02-28 16:14:01 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+import datetime
+import spotipy
+from bs4 import BeautifulSoup
+from spotipy.oauth2 import SpotifyOAuth
+
+header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
+website_link = "https://www.billboard.com/charts/hot-100/"
+song_uri = []
+
+#User inputs the date
+user_date = input("Enter the date (YYYY-MM-DD): ")
+try:
+    datetime.date.fromisoformat(user_date)
+except ValueError:
+    raise ValueError("Incorect date format, it should be YYYY-MM-DD.")
+year = user_date.split("-")[0]
+
+#spotipy authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="4b23e29a7cb94daeb07e07c596481c95",
+        client_secret="4f466ebe520544ad8a84d6773b01e3cc",
+        show_dialog=True,
+        cache_path="token.txt",
+        username="Gabi",
+    )
+)
+user_id = sp.current_user()["id"]
+
+#Web scraping the Billboard Hot 100 site
+response = requests.get(url=f"{website_link}/{user_date}/", headers=header)
+web_page = response.text
+
+soup = BeautifulSoup(web_page, "html.parser")
+songs = soup.select("li ul li h3")
+song_list = [song.getText().strip() for song in songs]
+
+#Extracting the song URIs
+for song in song_list:
+    results = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        song_uri.append(results["tracks"]["items"][0]["uri"])
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify")
+
+#Creates a new playlist on Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{user_date} Billboard 100", public=False)
+
+#Adding the songs to the playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uri)
